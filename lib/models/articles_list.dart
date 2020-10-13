3ecabe510c792db024a8f1c0332adb088b61196e
seemantshekhar43 <seemantshekhar43@gmail.com>
2020-10-13 20:15:56 +0530

    minor_change

--- lib/models/articles_list.dart
+++ lib/models/articles_list.dart
@@ -8,86 +8,11 @@ class ArticlesList {
 
   Future<void> fetchData()async {
     List<Article> list = await feedRepository.getNewsByCategory('general');
-    _list = [];
-    if(list.isNotEmpty){
+    if (list.isNotEmpty) {
+      _list.clear();
       _list.addAll(list);
+      print(_list.length);
     }
-  // _list = [
-  // Article(
-  // title:
-  // 'Post-COVID care: Government recommends guidelines for home care and recovery - Times of India',
-  // source: 'The Times of India',
-  // author: 'TIMESOFINDIA.COM',
-  // imageURL: 'https://static.toiimg.com/photo/78124262.cms',
-  // articleURL:
-  // 'https://timesofindia.indiatimes.com/life-style/health-fitness/health-news/post-covid-care-government-recommends-guidelines-for-home-care-and-recovery/photostory/78123951.cms',
-  // content:
-  // 'Post-COVID care is something which needs attention, even after a person has tested negative for the virus. A lot of COVID patients are suggested to practice exercises, especially breathing exercises to improve respiratory distress and relieve congestion, even when they are in recovery. Post a negative A lot of COVID patients are suggested to practice exercises, especially breathing exercises to improve respiratory distress and relieve congestion, even when they are in recovery. Post a negative A lot of COVID patients are suggested to practice exercises, especially breathing exercises to improve respiratory distress and relieve congestion, even when they are in recovery. Post a negative',
-  // publishedAt: DateTime.now(),
-  // ),
-  // Article(
-  // title:
-  // 'Post-COVID care: Government recommends guidelines for home care and recovery - Times of India',
-  // source: 'The Times of India',
-  // author: 'TIMESOFINDIA.COM',
-  // imageURL: 'https://static.toiimg.com/photo/78124262.cms',
-  // articleURL:
-  // 'https://timesofindia.indiatimes.com/life-style/health-fitness/health-news/post-covid-care-government-recommends-guidelines-for-home-care-and-recovery/photostory/78123951.cms',
-  // content:
-  // 'Post-COVID care is something which needs attention, even after a person has tested negative for the virus.',
-  // publishedAt: DateTime.now(),
-  // ),
-  // Article(
-  // title:
-  // 'Post-COVID care: Government recommends guidelines for home care and recovery - Times of India',
-  // source: 'The Times of India',
-  // author: 'TIMESOFINDIA.COM',
-  // imageURL: 'https://static.toiimg.com/photo/78124262.cms',
-  // articleURL:
-  // 'https://timesofindia.indiatimes.com/life-style/health-fitness/health-news/post-covid-care-government-recommends-guidelines-for-home-care-and-recovery/photostory/78123951.cms',
-  // content:
-  // 'Post-COVID care is something which needs attention, even after a person has tested negative for the virus.',
-  // publishedAt: DateTime.now(),
-  // ),
-  // Article(
-  // title:
-  // 'Post-COVID care: Government recommends guidelines for home care and recovery - Times of India',
-  // source: 'The Times of India',
-  // author: 'TIMESOFINDIA.COM',
-  // imageURL: 'https://static.toiimg.com/photo/78124262.cms',
-  // articleURL:
-  // 'https://timesofindia.indiatimes.com/life-style/health-fitness/health-news/post-covid-care-government-recommends-guidelines-for-home-care-and-recovery/photostory/78123951.cms',
-  // content:
-  // 'Post-COVID care is something which needs attention, even after a person has tested negative for the virus.',
-  // publishedAt: DateTime.now(),
-  // ),
-  // Article(
-  // title:
-  // 'Post-COVID care: Government recommends guidelines for home care and recovery - Times of India',
-  // source: 'The Times of India',
-  // author: 'TIMESOFINDIA.COM',
-  // imageURL: 'https://static.toiimg.com/photo/78124262.cms',
-  // articleURL:
-  // 'https://timesofindia.indiatimes.com/life-style/health-fitness/health-news/post-covid-care-government-recommends-guidelines-for-home-care-and-recovery/photostory/78123951.cms',
-  // content:
-  // 'Post-COVID care is something which needs attention, even after a person has tested negative for the virus.',
-  // publishedAt: DateTime.now(),
-  // ),
-  // Article(
-  // title:
-  // 'Post-COVID care: Government recommends guidelines for home care and recovery - Times of India',
-  // source: 'The Times of India',
-  // author: 'TIMESOFINDIA.COM',
-  // imageURL: 'https://static.toiimg.com/photo/78124262.cms',
-  // articleURL:
-  // 'https://timesofindia.indiatimes.com/life-style/health-fitness/health-news/post-covid-care-government-recommends-guidelines-for-home-care-and-recovery/photostory/78123951.cms',
-  // content:
-  // 'Post-COVID care is something which needs attention, even after a person has tested negative for the virus.',
-  // publishedAt: DateTime.now(),
-  // ),
-  //
-  //
-  // ];
   }
   Future<void> fetchDataByTopic(String topic) async{
     List<Article> list = await feedRepository.getNewsByTopic(topic);
@@ -99,6 +24,5 @@ class ArticlesList {
     }
   }
 
-  List<Article> get articles => _list;
-
+    List<Article> get articles => _list;
   }
